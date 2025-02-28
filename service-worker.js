0f0059e0a814ae6e156c0e45fc0b91d60636e834
Ardith-1989 <73720530+Ardith-1989@users.noreply.github.com>
2025-02-28 12:40:02 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "revision-randomiser-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "styles.css",
+  "script.js",
+  "manifest.json",
+  "cards_data.json",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
+];
+
+// Install the service worker and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch resources from cache when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache on activation
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
